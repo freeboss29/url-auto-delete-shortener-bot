9cce2365596b253d1c28878c18a5ef537dd989db
freeboss29 <140298202+freeboss29@users.noreply.github.com>
2023-08-06 09:51:10 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -381,7 +381,7 @@ async def get_shortlink(link):
     if "http" == https:
         https = "https"
         link = link.replace("http", https)
-    url = f'https://kpslink.in/api'
+    url = f'https://tnshort.net/api'
     params = {'api': URL_SHORTNER_WEBSITE_API,
               'url': link,
               }
